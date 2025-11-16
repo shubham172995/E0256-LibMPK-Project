c443645d3876cef81db770e32db4592f31c3b02d
shubhsy <shubham.wheofficial@gmail.com>
2025-11-16 23:30:45 +0530

    added another testcase which is commented for now

--- src/main.c
+++ src/main.c
@@ -102,5 +102,29 @@ int main() {
     free(msgs);
     ClearMappedPages();
 
+    //  ANOTHER TESTCASE. UNCOMMENT TO CHECK THAT EVEN WITHIN THIS MAIN, IF THERE ARE MULTIPLE USES OF PKEY_SET
+    //  LEGITIMATE AS WELL AS ILLEGITIMATE, BOTH ARE HANDLED.
+
+    /*
+    int pkey = pkey_alloc(0, 0);
+    if (pkey < 0) {
+        perror("pkey_alloc failed");
+        return 1;
+    }
+
+    printf("Allocated pkey = %d\n", pkey);
+
+    // Try setting permissions:
+    int ret = pkey_set(pkey, PKEY_DISABLE_WRITE);
+    if (ret < 0) {
+        perror("pkey_set failed");
+    } else {
+        printf("Successfully called pkey_set()\n");
+    }
+
+    // Free key
+    pkey_free(pkey);
+    */
+
     return 0;
 }
